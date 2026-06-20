c4ddfb8c2a4dccd37e10f3cde1b620dd6922980c
Javier Mejias <javier.mejias@kontrollererp.cl>
2026-06-19 21:37:13 -0400

    fix: configure alphatab worker scriptFile

--- src/hooks/useAlphaTab.ts
+++ src/hooks/useAlphaTab.ts
@@ -65,7 +65,8 @@ export function useAlphaTab(song: Song | null) {
     apiRef.current = new alphaTab.AlphaTabApi(containerRef.current, {
       core: {
         fontDirectory: 'https://cdn.jsdelivr.net/npm/@coderline/alphatab@latest/dist/font/',
-        useWorkers: false
+        scriptFile: 'https://cdn.jsdelivr.net/npm/@coderline/alphatab@latest/dist/alphaTab.mjs',
+        useWorkers: true
       },
       player: {
         enablePlayer: true,
